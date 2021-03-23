db72c5805959ba305977854d75478f5de205d53c
Ethan Stoneman <estoneman@Ethans-MacBook-Pro.local>
2021-03-22 23:31:09 -0500

    presentation mock-up

--- c/results.c
+++ c/results.c
@@ -1,6 +1,9 @@
-#include <errno.h>                    // errno values
+#include <errno.h>                    // error checking for libcurl
 #include <curl/curl.h>  
 #include "urldecoder.c"
+
+#define BODY "<body>"
+#define END_BODY "</body>"
 
 struct memory {
     char* response;
@@ -24,7 +27,8 @@ static size_t write_data(void *data, size_t size, size_t nmemb, void *stream) {
 	return realsize;
 }
 
-void parseURL(char* str) {
+// convert ASCII to UTF-8
+void parseForm(char* str) {
 
     int i = 0, j, n = strlen(str);
 
@@ -39,6 +43,16 @@ void parseURL(char* str) {
 
 }
 
+char* body(char *html) {
+    char *begin = strstr(html, BODY) + 6;
+    char *end = strstr(html, END_BODY);
+    size_t len = end - begin;
+    char *result = (char*)malloc(sizeof(char)*(len + 1));
+    strncpy(result, begin, len);
+    result[len] = '\0';
+    return result;
+}
+
 int main()
 {
 
@@ -51,54 +65,42 @@ int main()
     printf("Content-type: text/html\r\n\r\n");
     printf("<html>\n");
     printf("<head>\n");
-    printf("<title>\n");
-    printf("Results\n");
+    printf("<title>Results\n");
     printf("</title>\n");
     printf("</head>\n");
     printf("<body>\n");
 
-    /*--------------------------------------------------------------*/
-    /* The REQUEST_METHOD is "POST".  The environment variable      */
-    /* CONTENT_LENGTH will tell us how many bytes of data to read   */
-    /* from stdin.  Note: CONTENT_LENGTH must be convert to an int. */
-    /*--------------------------------------------------------------*/
+    // content length is the amount of bytes that are being read
     contentLenString = getenv("CONTENT_LENGTH");
     contentLength = atoi(contentLenString);
 
     if ( contentLength ) {
 
-        printf("<h4>Content Length<h4>%i\n", contentLength);
-
-        // Allocate and set memory to read stdin data into
+        // Allocate and set memory to read stdin data into (cgi form input)
         stdInData = malloc(contentLength);
         if ( stdInData )
             memset(stdInData, 0x00, contentLength);
         else
             printf("ERROR: Unable to allocate memory\n");
-        /*----------------------------------------------------------*/
-        /* A CGI program MUST read standard input as a stream       */
-        /* file only up to and including CONTENT_LENGTH bytes.      */
-        /* Never should a program read more than CONTENT_LENGTH     */
-        /* bytes.  A CGI program that reads standard input must     */
-        /* never depend on an end of file flag.  This will cause    */
-        /* unpredictable results when the CGI program reads         */
-        /* standard input.                                          */
-        /*----------------------------------------------------------*/
-        printf("<h4>URL:</h4>\n");
+
+        // this calculates the amount of bytes that was successfully read into memory
         bytesRead = fread((char*)stdInData, 1, contentLength, stdin);
 
         // If we successfully read all bytes from stdin, format and
         // write the data to stdout using the writeData function.
 
-        parseURL(stdInData);
+        parseForm(stdInData);
 
+        // if these do not match then there was an error reading the data
         if ( bytesRead != contentLength )
             printf("<br>Error reading standard input\n");
 
+        // decode ASCII url to UTF-8 to be able to search a url e.g. https%3A%2F%2Fgoogle.com --> https://google.com
         char* url = malloc(strlen(stdInData) + 1);
         urldecode2(url, stdInData);
 
         // Free the storage allocated to hold the stdin data
+        free(stdInData);
 
         CURL *curl_handle;
 
@@ -131,21 +133,18 @@ int main()
         if ( !(res = curl_easy_perform(curl_handle)) )
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 
-        printf("<br>%lu bytes retrieved\n", (unsigned long)chunk.size);
+        // printf("<br>%lu bytes retrieved<br>\n", (unsigned long)chunk.size);
 
-        unsigned int i;
-        for (i = 0; i < (unsigned int)chunk.size; i++)
-            printf("%c", *(chunk.response + i));
-
-        // cleanup any memory leaks
-        curl_easy_cleanup(curl_handle);
+        chunk.response = body(chunk.response);
+        while (*chunk.response) printf("%c\n", *chunk.response++);
 
         // free any memory used to store input data
         free(chunk.response);
+	    free(url);
 
+        // cleanup any memory leaks
+        curl_easy_cleanup(curl_handle);
         curl_global_cleanup();
-
-        free(stdInData);
 
     }   
     else
